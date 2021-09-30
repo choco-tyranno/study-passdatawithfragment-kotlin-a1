c2fe4a855062e45665b7f78192d189b5654ddb97
choco-tyranno <choco.tyranno@gmail.com>
2021-09-30 21:55:42 +0900

    feat: add passing data code using ActivityResultContracts API.

--- LegacyWayFragmentDataPass/app/src/main/java/com/choco_tyranno/legacywayfragmentdatapass/FirstFragment.kt
+++ LegacyWayFragmentDataPass/app/src/main/java/com/choco_tyranno/legacywayfragmentdatapass/FirstFragment.kt
@@ -6,21 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultRegistry
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.os.bundleOf
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.setFragmentResult
 import androidx.navigation.findNavController
 
 class FirstFragment : Fragment(R.layout.fragment_first) {
+    val getContent = registerForActivityResult(ActivityResultContracts.GetContent()){uri ->
+        val imageView = view?.findViewById<ImageView>(R.id.image)
+        imageView?.setImageURI(uri)
+    }
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val firstButton = view.findViewById<Button>(R.id.first_button)
 
         firstButton.setOnClickListener {
-            setFragmentResult("requestKey"
-                , bundleOf("data" to "Hi")
-            )
-            firstButton.findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+            //MIME TYPE
+            getContent.launch("image/*")
         }
     }
 }
